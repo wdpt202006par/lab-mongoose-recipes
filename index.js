0a4cfaea61e12e1adbee790b0a7db884cb3363a5
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-08-30 22:31:14 +0200

    iteration 3 done

--- index.js
+++ index.js
@@ -1,69 +1,72 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-
   .then(() => {
-  Recipe.insertMany(data)
-})
-  .then(()=> {
-  console.log(Recipe.find({}, "title"))
-})
-  .catch(error => {
-    console.log('no titles displayed', error)
+    Recipe.insertMany(data)
+      .then(() => {
+        Recipe.find({})
+          .then((recipes) => {
+            recipes.forEach(el => {
+              console.log(el.title);
+            })
+          })
+          .catch((error) => {
+            console.error("Error .find()", error);
+          })
+      })
   })
-  
-  // TO CHECK 
-  //   Recipe.create({
-  //       "title": "Asian Glazed Chicken Thighs",
-  //       "level": "Amateur Chef",
-  //       "ingredients": [
-  //         "1/2 cup rice vinegar",
-  //         "5 tablespoons honey",
-  //         "1/3 cup soy sauce (such as Silver Swan®)",
-  //         "1/4 cup Asian (toasted) sesame oil",
-  //         "3 tablespoons Asian chili garlic sauce",
-  //         "3 tablespoons minced garlic",
-  //         "salt to taste",
-  //         "8 skinless, boneless chicken thighs"
-  //       ],
-  //       "cuisine": "Asian",
-  //       "dishType": "main_course",
-  //       "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-  //       "duration": 40,
-  //       "creator": "Chef LePapu"
-  //     })
-  //     .then(() => {
-  //       Recipe.find({})
-  //         .then(function (recipe) { // ATTENTION : renvoi un tableau (array)
-  //           console.log("Title: ", recipe[0].title);
-  //         })
-  //         .catch(function (error) {
-  //           console.log("no title found", error);
-  //         })
-  //     })
-  //     .catch(error => {
-  //       console.log('pas cool', error);
-  //     })
-  // })
-  
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// TO CHECK
+//   Recipe.create({
+//       "title": "Asian Glazed Chicken Thighs",
+//       "level": "Amateur Chef",
+//       "ingredients": [
+//         "1/2 cup rice vinegar",
+//         "5 tablespoons honey",
+//         "1/3 cup soy sauce (such as Silver Swan®)",
+//         "1/4 cup Asian (toasted) sesame oil",
+//         "3 tablespoons Asian chili garlic sauce",
+//         "3 tablespoons minced garlic",
+//         "salt to taste",
+//         "8 skinless, boneless chicken thighs"
+//       ],
+//       "cuisine": "Asian",
+//       "dishType": "main_course",
+//       "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+//       "duration": 40,
+//       "creator": "Chef LePapu"
+//     })
+//     .then(() => {
+//       Recipe.find({})
+//         .then(function (recipe) { // ATTENTION : renvoi un tableau (array)
+//           console.log("Title: ", recipe[0].title);
+//         })
+//         .catch(function (error) {
+//           console.log("no title found", error);
+//         })
+//     })
+//     .catch(error => {
+//       console.log('pas cool', error);
+//     })
+// })
